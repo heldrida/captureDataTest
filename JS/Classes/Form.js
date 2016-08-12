eec45566875860177a0285602b40cb8b4a5fcdf4
Helder <heldrida@gmail.com>
2016-08-12 01:26:33 +0100

    iterate the mappings list and set event listeners for eah element based on its attributes; created handler that takes attributes and retrieves the data from the element correctly

--- JS/Classes/Form.js
+++ JS/Classes/Form.js
@@ -17,6 +17,7 @@ Form.prototype = {
 	init: function (params) {
 
 		this.setProperties(params);
+		this.setEventListeners();
 
 	},
 
@@ -24,13 +25,51 @@ Form.prototype = {
 
 		this.mappings = params.mappings;
 		this.dataReporter = params.dataReporter;
-
-
+		this.form = document.querySelector('form');
+		this.mappedEvents = { onChange: 'change', onLoad: 'load' };
 
 	},
 
 	setEventListeners: function () {
 
+		_.forEach(this.mappings, function (obj, key) {
+
+			var el = document.querySelector(obj.selector);
+
+			if (el) {
+
+				el.addEventListener(this.mappedEvents[obj.event], function (e) {
+
+					this.userInputHandler.call(this, { el: e.target, attrs: obj });
+
+				}.bind(this));
+
+			}
+
+		}.bind(this));
+
+	},
+
+	userInputHandler: function (params) {
+
+		var el = params.el;
+		var data = (function (attribute) {
+
+			switch (attribute) {
+				case 'value':
+					data = el.value;
+					break;
+				case 'text':
+					data = el.text;
+					break;
+				default:
+					data = '';
+					break;
+			}
+
+			return data;
+
+		}(params.attrs.attribute));
 
 
 	}
--- dist/bundle.js
+++ dist/bundle.js
@@ -237,6 +237,7 @@
 		init: function (params) {
 
 			this.setProperties(params);
+			this.setEventListeners();
 
 		},
 
@@ -244,13 +245,51 @@
 
 			this.mappings = params.mappings;
 			this.dataReporter = params.dataReporter;
-
-
+			this.form = document.querySelector('form');
+			this.mappedEvents = { onChange: 'change', onLoad: 'load' };
 
 		},
 
 		setEventListeners: function () {
 
+			_.forEach(this.mappings, function (obj, key) {
+
+				var el = document.querySelector(obj.selector);
+
+				if (el) {
+
+					el.addEventListener(this.mappedEvents[obj.event], function (e) {
+
+						this.userInputHandler.call(this, { el: e.target, attrs: obj });
+
+					}.bind(this));
+
+				}
+
+			}.bind(this));
+
+		},
+
+		userInputHandler: function (params) {
+
+			var el = params.el;
+			var data = (function (attribute) {
+
+				switch (attribute) {
+					case 'value':
+						data = el.value;
+						break;
+					case 'text':
+						data = el.text;
+						break;
+					default:
+						data = '';
+						break;
+				}
+
+				return data;
+
+			}(params.attrs.attribute));
 
 
 		}
